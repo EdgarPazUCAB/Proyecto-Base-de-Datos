a0245f761bb941e5fcbfd2b75065ed069fea24a2
EdgarPazUCAB <epaz01353@gmail.com>
2026-06-27 15:35:50 -0400

    Scripts de UPDATE agregados.

---
+++ UPDATES Base de datos.sql
@@ -0,0 +1,31 @@
+UPDATE miembro 
+SET correo_institucional = 'ricardo.vargas@egr.ucab.edu.ve'
+WHERE cedula_miembro = 'V-89012345';
+
+UPDATE miembro
+SET correo_institucional = 'ana.lopez@est.ucab.edu.ve'
+WHERE cedula_miembro = 'V-12345678';
+
+UPDATE miembro
+SET correo_institucional = 'carlos.perez@egr.ucab.edu.ve'
+WHERE cedula_miembro = 'V-23456789';
+
+UPDATE miembro
+SET correo_institucional = 'maria.sanches@adm.ucab.edu.ve'
+WHERE cedula_miembro = 'V-34567890';
+
+UPDATE miembro
+SET correo_institucional = 'pedro.martinez@prof.ucab.edu.ve'
+WHERE cedula_miembro = 'V-45678901';
+
+UPDATE miembro
+SET correo_institucional = 'laura.beatriz@est.ucab.edu.ve'
+WHERE cedula_miembro = 'V-56789012';
+
+UPDATE miembro
+SET correo_institucional = 'jorge.dominguez@est.ucab.edu.ve'
+WHERE cedula_miembro = 'V-67890123';
+
+UPDATE miembro
+SET correo_institucional = 'valentina.rojas@est.ucab.edu.ve'
+WHERE cedula_miembro = 'V-78901234';
